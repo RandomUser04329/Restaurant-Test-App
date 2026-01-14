02968dbcfe64d4b8afa14b574df99980e6c5c90e
2sided <rsalinas24z@yahoo.com>
2026-01-14 12:21:12 -0600

    Added Text Content to the contact tab + icons

--- src/restaurantContact.js
+++ src/restaurantContact.js
@@ -1,19 +1,63 @@
+import TikTokLogo from "../images/TikTokLogo.png";
+import InstagramLogo from "../images/InstagramLogo.png";
+
+//Main Page Boxes + header
 const ContactDiv = document.createElement("div");
-ContactDiv.className = "ContactDiv";
-const ContactHeader = document.createElement("h1");
-ContactHeader.id = "ContactHeader";
-ContactHeader.textContent = "Our Contact's";
+    ContactDiv.className = "ContactDiv";
+    const ContactHeader = document.createElement("h1");
+        ContactHeader.id = "ContactHeader";
+        ContactHeader.textContent = "Contact us";
 
 const ContactMainDiv = document.createElement("div");
-ContactMainDiv.className = "ContactMainDiv";
+    ContactMainDiv.className = "ContactMainDiv";
 
+//Phone Boxes + Headers
 const PhoneContactDiv = document.createElement("div");
-PhoneContactDiv.className = "PhoneContactDiv";
-const PhoneContactHeader = document.createElement("h1");
+    PhoneContactDiv.className = "PhoneContactDiv";
+    const ContactBox = document.createElement("div"); 
+        ContactBox.id = "PhoneBox"; 
+    const PhoneContactHeader = document.createElement("h1");
+        PhoneContactHeader.id = "PhoneContactHeader";
+        PhoneContactHeader.textContent = "Phone Contact:"
+    const PhoneContactPara = document.createElement("p");
+        PhoneContactPara.id = "PhonePara";
+        PhoneContactPara.textContent = "555-555-5555";
+    const EmailContactHeader = document.createElement("h1");
+        EmailContactHeader.id = "EmailHeader";
+        EmailContactHeader.textContent = "Email Contact:";
+    const EmailContactPara = document.createElement("p");
+        EmailContactPara.id = "EmailPara";
+        EmailContactPara.textContent = "FakeEmail@Hotmail.com"
 
+
+//Social Media boxes + header
 const OrderHereDiv = document.createElement("div");
-OrderHereDiv.className = "OrderHereDiv";
-const OrderHeader = document.createElement("h1");
+    OrderHereDiv.className = "OrderHereDiv";
+    const OrderBox = document.createElement("div");
+        OrderBox.id = "OrderBox";
+    const OrderHeader = document.createElement("h1");
+        OrderHeader.id = "OrderHeader";
+        OrderHeader.textContent = "Follow our Socials!"
+//Instagram data
+    const InstaHeader = document.createElement("h1");
+        InstaHeader.id = "InstaHeader";
+        InstaHeader.textContent = "Instagram:"
+    const InstaLogo = document.createElement("img");
+        InstaLogo.src = InstagramLogo;
+        InstaLogo.id = "InstaLogo";
+    const InstaPara = document.createElement("p");
+        InstaPara.id = "InstaPara";
+        InstaPara.textContent = "TheRealBonezBurgerz"
+//Tik tok Data
+    const TikTokHeader = document.createElement("h1");
+        TikTokHeader.id = "TikTokHeader";
+        TikTokHeader.textContent = "TikTok:"
+    const TTLogo = document.createElement("img");
+        TTLogo.src = TikTokLogo
+        TTLogo.id = "TikTokLogo";
+    const TikTokPara = document.createElement("p");
+        TikTokPara.id = "TikTokPara";
+        TikTokPara.textContent = "TheRealBonezBurgerz"
 
 
 
@@ -22,6 +66,24 @@ ContactDiv.appendChild(ContactMainDiv);
 
 ContactMainDiv.appendChild(PhoneContactDiv);
 ContactMainDiv.appendChild(OrderHereDiv);
+
+PhoneContactDiv.appendChild(ContactBox);
+ContactBox.appendChild(PhoneContactHeader);
+ContactBox.appendChild(PhoneContactPara);
+ContactBox.appendChild(EmailContactHeader);
+ContactBox.appendChild(EmailContactPara);
+
+OrderHereDiv.appendChild(OrderBox);
+OrderBox.appendChild(OrderHeader);
+
+OrderBox.appendChild(InstaHeader);
+OrderBox.appendChild(InstaPara);
+InstaPara.appendChild(InstaLogo);
+
+OrderBox.appendChild(TikTokHeader);
+OrderBox.appendChild(TikTokPara);
+TikTokPara.appendChild(TTLogo);
+
 
 export {
     ContactDiv
--- src/restaurantMenu.js
+++ src/restaurantMenu.js
@@ -3,7 +3,7 @@ const MenuDiv = document.createElement("div");
 MenuDiv.className = "MenuDiv";
 const MenuHeader = document.createElement("h1");
 MenuHeader.id = "MenuHeader";
-MenuHeader.textContent = "Our Menu's"
+MenuHeader.textContent = "Our Menu"
 
 //Menus Divs + Names
 const MenuSection = document.createElement("div");
