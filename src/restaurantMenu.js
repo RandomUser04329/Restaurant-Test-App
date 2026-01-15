e5fac7caf9194509811cf072632d9b5b265a6596
Rob <rsalinas24z@yahoo.com>
2026-01-15 12:28:44 -0600

    Bunch of content + styles to menu

--- src/restaurantMenu.js
+++ src/restaurantMenu.js
@@ -3,7 +3,7 @@ const MenuDiv = document.createElement("div");
 MenuDiv.className = "MenuDiv";
 const MenuHeader = document.createElement("h1");
 MenuHeader.id = "MenuHeader";
-MenuHeader.textContent = "Our Menu"
+MenuHeader.textContent = "Our Menu";
 
 //Menus Divs + Names
 const MenuSection = document.createElement("div");
@@ -12,10 +12,10 @@ MenuSection.className = "Menus";
 const AllDaySection = document.createElement("div");
 AllDaySection.className = "AllDayMenuDiv";
     const AllDayBox = document.createElement("div");
-    AllDayBox.className = "AlldayMenuBox";
+    AllDayBox.className = "AllDayMenuBox";
         const AllDayBoxHeader = document.createElement("h1");
         AllDayBoxHeader.id = "AllDayBoxHeader";
-        AllDayHeader.textContent = "All Day";
+        AllDayBoxHeader.textContent = "All Day";
     const AllDayMenuDetails = document.createElement("div");
     AllDayMenuDetails.className = "AllDayMenuDetailsBox";
         const AllDayDetailsPara = document.createElement("p");
@@ -23,32 +23,69 @@ AllDaySection.className = "AllDayMenuDiv";
     
 
 
-const BeveragesSection = document.createElement("div");
-BeveragesSection.className = "BeveragesMenuDiv"; 
-    const BeveragesBox = document.createElement("div");
-    BeveragesBox.className = "BeveragesMenuBox";
-        const BeveragesBoxHeader = document.createElement("h1");
-        BeveragesBoxHeader.id = "BeveragesBoxHeader";
-    const BeveragesMenuDetails = document.createElement("div");
-    BeveragesMenuDetails.className = "BeveragesrMenuDetailsBox";
-        const BeveragesDetailsPara = document.createElement("p");
-        BeveragesDetailsPara.id = "BeveragesDetailsPara";
+const SidesDrinksSection = document.createElement("div");
+SidesDrinksSection.className = "SidesDrinksMenuDiv"; 
+    const SidesBox = document.createElement("div");
+    SidesBox.className = "SidesMenuBox";
+        const SidesBoxHeader = document.createElement("h1");
+        SidesBoxHeader.id = "SidesBoxHeader";
+        SidesBoxHeader.textContent = "Sides"
+    const SidesMenuDetails = document.createElement("div");
+    SidesMenuDetails.className = "SidesMenuDetailsBox";
+        const SidesDetailsItems = document.createElement("p");
+        SidesDetailsItems.id = "SidesDetailsItems";
+        SidesDetailsItems.textContent = 
+            "Thick Cut Fries\n" + 
+            "Curly Fries\n" + 
+            "Fried Pickles\n" + 
+            "Stuffed Jalapeno Peppers\n";
+        const SidesDetailsPrices = document.createElement("p");
+            SidesDetailsPrices.id = "SidesDetailsPrices";
+            SidesDetailsPrices.textContent = 
+            "$1.29\n" +
+            "$1.29\n" + 
+            "$1.49\n" + 
+            "$1.69\n";
+
+    const DrinksBox = document.createElement("div");
+    DrinksBox.className = "DrinksMenuBox";
+        const DrinksBoxHeader = document.createElement("h1");
+        DrinksBoxHeader.id = "DrinksBoxHeader";
+        DrinksBoxHeader.textContent = "Beverages"; 
+        const DrinksMenuDetails = document.createElement("div");
+        DrinksMenuDetails.className = "DrinksMenuDetailsBox";
+            const DrinksDetailsItems = document.createElement("p");
+            DrinksDetailsItems.id = "DrinksDetailsItems";
+            DrinksDetailsItems.textContent = 
+            "Fountain Drink\nUn-Sweet Tea\nSweet Tea\nBudLight\nModelo\nMichelob\nWater"; 
+            const DrinksDetailsPrices = document.createElement("p");
+            DrinksDetailsPrices.id = "DrinksDetailsPrices";
+
+
+
 
         
 MenuDiv.appendChild(MenuHeader);
 MenuDiv.appendChild(MenuSection);
 MenuSection.appendChild(AllDaySection);
-MenuSection.appendChild(BeveragesSection);
+MenuSection.appendChild(SidesDrinksSection);
 
 AllDaySection.appendChild(AllDayBox);
 AllDayBox.appendChild(AllDayBoxHeader);
 AllDayBox.appendChild(AllDayMenuDetails);
 AllDayMenuDetails.appendChild(AllDayDetailsPara);
 
-BeveragesSection.appendChild(BeveragesBox);
-BeveragesBox.appendChild(BeveragesBoxHeader);
-BeveragesBox.appendChild(BeveragesMenuDetails);
-BeveragesMenuDetails.appendChild(BeveragesDetailsPara);
+SidesDrinksSection.appendChild(SidesBox);
+SidesBox.appendChild(SidesBoxHeader);
+SidesBox.appendChild(SidesMenuDetails);
+SidesMenuDetails.appendChild(SidesDetailsItems);
+SidesMenuDetails.appendChild(SidesDetailsPrices);
+
+SidesDrinksSection.appendChild(DrinksBox);
+DrinksBox.appendChild(DrinksBoxHeader);
+DrinksBox.appendChild(DrinksMenuDetails);
+DrinksMenuDetails.appendChild(DrinksDetailsItems);
+DrinksMenuDetails.appendChild(DrinksDetailsPrices);
 
 
 
